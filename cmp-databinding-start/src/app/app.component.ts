7f67ddefa94bb3d0947c5c483e5c095d0f663ca2
SpeedForce4all <82789392+SpeedForce4all@users.noreply.github.com>
2021-05-25 12:50:27 -0500

    Prior
    Before Actually doing assignment

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -16,6 +16,14 @@ export class AppComponent {
      });
   }
 
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
+
   onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
       type: 'blueprint',
