503bc1877660814f4d81763f3fbc447723d143d9
SpeedForce4all <82789392+SpeedForce4all@users.noreply.github.com>
2021-05-25 14:19:07 -0500

    Direction in app-html

--- cmp-databinding-start/src/app/app.module.ts
+++ cmp-databinding-start/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GameOddComponent } from './game-odd/game-odd.component';
 import { GameEvanComponent } from './game-evan/game-evan.component';
+import { ControlComponent } from './control/control.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameOddComponent,
     GameEvanComponent,
+    ControlComponent,
   ],
   imports: [
     BrowserModule,
